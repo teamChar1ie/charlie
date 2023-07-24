fbbfff59bfcb5acaed10f355ed1f754e3b53d1d5
Yurii Shmal <teamcharlie61@gmail.com>
2023-07-24 13:46:07 +0300

    Making the logo a link to the main page.

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 const Logo = () => {
     const router = useRouter();
 
     return (
+        <Link href={'https://charlie-nine.vercel.app/'}>
         <Image 
             alt='Logo'
             className='hidden md:block cursor-pointer'
@@ -14,6 +16,7 @@ const Logo = () => {
             width='100'
             src='/images/logo1.png'
         />
+        </ Link>
     )
 }
 
