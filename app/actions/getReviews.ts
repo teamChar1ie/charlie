8b5abbb3f127601205b45e3c01100fd837f35b90
Yurii Shmal <teamcharlie61@gmail.com>
2023-08-18 14:08:39 +0300

    Get all the new elements on. Nice.

---
+++ app/actions/getReviews.ts
@@ -0,0 +1,55 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+    listingId?: string;
+    userId?: string;
+    authorId?: string;
+}
+
+export default async function getReservations(
+  params: IParams
+) {
+  try {
+    const { listingId, userId, authorId } = params;
+
+    const query: any = {};
+
+    if (listingId) {
+        query.listingId = listingId;
+      };
+  
+      if (userId) {
+        query.userId = userId;
+      }
+  
+      if (authorId) {
+        query.listing = { userId: authorId };
+      }
+
+    const reviews = await prisma.review.findMany({
+      where: query,
+      include: {
+        listing: true
+      }
+    });
+
+    const safeReviews = reviews.map(
+      (review) => ({
+      ...review,
+      professionalCompetence: review.professionalCompetence,
+      reliability: review.reliability,
+      interpersonalSkills: review.interpersonalSkills,
+      trustworthiness: review.trustworthiness,
+      overallImage: review.overallImage,
+      briefStatement: review.briefStatement,
+      listing: {
+        ...review.listing,
+        createdAt: review.listing.createdAt.toISOString(),
+      }
+    }));
+
+    return safeReviews;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
---
+++ app/components/ReviewBox.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+interface ReviewBoxProps {
+    professionalCompetence: number,
+    reliability: number;
+    interpersonalSkills: number;
+    trustworthiness: number;
+    overallImage: number; 
+    briefStatement: string;
+};
+
+const ReviewBox: React.FC<ReviewBoxProps> = ({ 
+    professionalCompetence,
+    reliability,
+    interpersonalSkills,
+    trustworthiness,
+    overallImage,
+    briefStatement}) => {
+  return ( 
+    <div
+      className="
+        max-w-[2520px]
+        mx-auto
+        xl:px-20 
+        md:px-10
+        sm:px-2
+        px-4
+      "
+    >
+    Professional Competence: ${professionalCompetence}
+
+    Reliability:  ${reliability}
+
+    Interpersonal Skills:  ${interpersonalSkills}
+
+    Trustworthiness:  ${trustworthiness}
+
+    Overall Image:  ${overallImage}
+
+    Brief Statement:  ${briefStatement}
+
+    </div>
+   );
+}
+ 
+export default ReviewBox;
